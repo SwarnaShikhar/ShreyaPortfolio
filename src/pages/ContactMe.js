2fd6068dd939c62d53baff6b399da642c7413fd9
Soumwadeep Guha <soumwadeep@gmail.com>
2023-02-15 06:06:25 +0530

    Fixed Designs

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Card from "../components/Card"
+import React from "react";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 import { GrSend } from "react-icons/gr";
 
 export default function ContactMe() {
   return (
-    <div className='box'>
+    <div className="box">
       <Navbar />
       <div className="divmain">
         <div className="divl">
@@ -13,29 +13,64 @@ export default function ContactMe() {
         </div>
         <div className="divr">
           <h5>Hey! you can contact me here..</h5>
-          <form action='https://formspree.io/f/xrgvpjno' method='POST'>  
+          <form action="https://formspree.io/f/xrgvpjno" method="POST" className="formbg">
             <div className="mb-3">
-              <label for="exampleInputName" className="form-label">Your Name</label>
-              <input type="text" name="Name" className="form-control" id="exampleInputName" />
+              <label for="exampleInputName" className="form-label">
+                Your Name
+              </label>
+              <input
+                type="text"
+                name="Name"
+                className="form-control"
+                id="exampleInputName"
+              />
               <div id="yourName" className="form-text"></div>
             </div>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" name="Email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <label for="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="Email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
               <div id="emailHelp" className="form-text"></div>
             </div>
             <div className="mb-3">
-              <label for="exampleInputNumber" className="form-label">Your Phone Number</label>
-              <input type="text" name="number" className="form-control" id="exampleInputNumber" />
+              <label for="exampleInputNumber" className="form-label">
+                Your Phone Number
+              </label>
+              <input
+                type="text"
+                name="number"
+                className="form-control"
+                id="exampleInputNumber"
+              />
             </div>
             <div class="form-group">
               <label for="exampleFormControlTextarea1">Your Message</label>
-              <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea
+                name="message"
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
             </div>
-            <button type="submit" id='submit' value="send" className="btn btn-primary"><GrSend />&emsp;Send</button>
+            <button
+              type="submit"
+              id="submit"
+              value="send"
+              className="btn btn-primary subbtn"
+            >
+              <GrSend style={{color:"white"}}/>
+              &emsp;Send
+            </button>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
